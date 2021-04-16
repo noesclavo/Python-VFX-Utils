0b5b5cde43b3285f9f7d17c386273d2ca6fc6994
Miles Essmiller <miles@noesclavo.com>
2021-04-16 09:12:31 -0700

    Updated dl_watch

--- m_lib/misc.py
+++ m_lib/misc.py
@@ -16,17 +16,19 @@ class Colour(object):
 
 class Misc(object):
     def __init__(self):
-        pass
+        self.p = Print()
 
     def ls(self, dir):
         import os
         import re
+        print(dir)
 
         if os.path.isdir(dir):
             items = []
             for item in os.listdir(dir):
-                if not re.match('\.', item):
+                if not item.startswith('.'):
                     items.append(item)
+
         return items
 
     def miles_time(self):
@@ -53,15 +55,49 @@ class Misc(object):
         if not os.path.exists(dirname):
             try:
                 os.mkdir(dirname, 0o777)
-            except OSError as e:
-                print("OS Error error({0}): {1}".format(e.errno, e.strerror))
-                raise
-            except IOError as e:
-                print("I/O Error error({0}): {1}".format(e.errno, e.strerror))
-                raise
+            except (OSError, ValueError) as e:
+                print(f'Error: {sys.exc_info()[0]}, os.mkdir({dirname}, 0o777) : {e.args}')
+                info = sys.exc_info()[1]
+                m.error(f'Error: {info}')
+                print(f'\tos.mkdir(\n\t\t{d}, 0o777)')
+                return
             except:
                 print('Unknown Error making directory: {}'.format(dirname))
                 raise
+
+    def find_common(self, item_list, **kwargs):
+        import re
+        from difflib import SequenceMatcher
+
+        junk = False
+        items = item_list
+        inum = len(items)
+
+        substring = None
+        match = None
+        for i in range(0, inum):
+            for j in range(i+1, inum):
+                s1 = items[i]
+                s2 = items[j]
+
+                if re.search(r'(\.\w?\.)', s1):
+                    match = SequenceMatcher(lambda x : x == re.search(r'(\.\w?\.)', s1).group(0), s1, s2).find_longest_match(
+                        0, len(s1),
+                        0, len(s2)
+                    )
+                else:
+                    print(s1)
+                    match = SequenceMatcher(None, s1, s2).find_longest_match(
+                        0, len(s1),
+                        0, len(s2)
+                    )
+
+                substring = s1[match.a: match.a + match.size]
+
+        self.p.success('Substring: {}'.format(substring), ':')
+
+        return substring
+
 
 
 class Print(object):
@@ -69,49 +105,58 @@ class Print(object):
     def __init__(self):
         pass
 
-    def success(self, string):
-        d = ':'
-        msg = string.split(d)
-        print('{0}{1}{2}{3}{4}{5}'.format(
+    def success(self, string, *args):
+        msg = string
+        intro = ''
+        delimeter = ''
+        if len(args) > 0:
+            delimeter = args[0]
+            (intro, msg)= msg.split(delimeter)
+            intro += Colour().reset
+
+
+        print('{0}{1}{2}{3}{4}'.format(
                                     Colour().green,
                                     Colour().bold,
-                                    msg[0],
-                                    Colour().reset,
-                                    d,
-                                    msg[1])
+                                    intro,
+                                    delimeter,
+                                    msg)
         )
 
-    def warn(self, string):
-        d = ':'
-        msg = string.split(d)
-        print('{0}{1}{2}{3}{4}{5}'.format(
+    def warn(self, string, *args):
+        msg = string
+        intro = ''
+        delimeter = ''
+        if len(args) > 0:
+            delimeter = args[0]
+            (intro, msg)= msg.split(delimeter)
+            intro += Colour().reset
+
+        print('{0}{1}{2}{3}{4}'.format(
                                     Colour().yellow,
                                     Colour().bold,
-                                    msg[0],
-                                    Colour().reset,
-                                    d,
-                                    msg[1])
+                                    intro,
+                                    delimeter,
+                                    msg)
+
         )
 
-    def error(self, string):
-        d = ':'
-        msg = string.split(d)
-        print('{0}{1}{2}{3}{4}{5}'.format(
+    def error(self, string, *args):
+        msg = string
+        intro = ''
+        delimeter = ''
+        if len(args) > 0:
+            delimeter = args[0]
+            intro= msg.split(delimeter)[0]
+            msg= ':'.join(msg.split(delimeter)[1:])
+            intro += Colour().reset
+
+        print('{0}{1}{2}{3}{4}'.format(
                                     Colour().red,
                                     Colour().bold,
-                                    msg[0],
-                                    Colour().reset,
-                                    d,
-                                    msg[1])
-        )
+                                    intro,
+                                    delimeter,
+                                    msg)
 
-    def print_self(self):
-        print('{0}{1}{2}{3}{4}'.format(
-                                Colour().bold,
-                                Colour().blue,
-                                Colour().underline,
-                                inspect.stack()[1][3],
-                                Colour().reset)
         )
-
 
